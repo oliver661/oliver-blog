42864d4262cef65f9c8fad0e90308c6804c5aee7
OliverM <oliver661@gmail.com>
2019-01-03 22:54:59 +0900

    Implement getContent.

--- page-class.php
+++ page-class.php
@@ -5,10 +5,13 @@ class Page{
 	public $mdname="";
 	public $category="none";
 	public $dir="";
+
 	private $path="";
 
 	public $date="";
+	public $size=0;
 	public $title="";
+	public $cp=-1;
 
 	function __construct($pn, $n, $c, $d="pages"){
 		$this->number=$pn;
@@ -20,20 +23,38 @@ class Page{
 		// stat file
 		$stat=stat($this->path);
 		if(!$stat){
-			throw new Exception("Failed to stat markdown file.");
+			throw new Exception("Failed to stat page file.", 500);
 		}
+		$this->date=date('Y-m-d H:i:s (e/O)', stat['mtime']);
+		$this->size=stat['size'];
 
 		// open and read
 		if(($h=fopen($this->path, 'r'))!==false){
 			while(($data=fgets($h))!==false){
 				$this->title=$data;
+				$this->cp=ftell($h);
+				break;
 			}
 			fclose($h);
+		}else{
+			throw new Exception("Failed to open page file", 500);
 		}
 
 	}
 
-	function getContent($hasTitle=false, $isFull=true){}
+	function getContent($hasTitle=false, $isFull=true){
+
+		if(!file_exists($this->path)){
+			throw new Exception("Page file not found.", 404);
+		}
+
+		// open and read
+		if(($c=file_get_contents($this->path, false, NULL, $this->cp))!==false){
+			return $c;
+		}else{
+			throw new Exception("Failed to read page file", 500);
+		}
+	}
 
 }
 ?>
